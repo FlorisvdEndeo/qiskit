3cb07429f442e0d2a2c0a862d26b71668120d928
FlorisvdEndeo <39434391+FlorisvdEndeo@users.noreply.github.com>
2020-05-26 21:41:24 +0200

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,14 @@
-
+from typing import Union, List, Optional, Callable
+import numpy as np
+from qiskit.circuit import QuantumCircuit
+from qiskit.providers import BaseBackend
+from qiskit.aqua import QuantumInstance
+from qiskit.aqua.algorithms import VQE
+from qiskit.aqua.operators import LegacyBaseOperator, Z2Symmetries
+from qiskit.aqua.components.optimizers import Optimizer
+from qiskit.aqua.components.variational_forms import VariationalForm
+from qiskit.aqua.utils.validation import validate_min, validate_in_set
+from .q_equation_of_motion import QEquationOfMotion
 
 def diffuser(circuit):
     """Apply inversion about the average step of Grover's algorithm."""
